ada158c0937c4205dd3399fbb98fa86ee870a55c
Administrator <Administrator@USER-20190615YV>
2019-10-11 15:35:06 +0800

    提交

--- src/helloword/HelloWorld.java
+++ src/helloword/HelloWorld.java
@@ -3,6 +3,6 @@ package helloword;
 public class HelloWorld {
  
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("hello world");
     }
 }
